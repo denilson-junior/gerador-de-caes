94f747ea669706c03413e39efed18c557382c1b2
Denilson Souza Gomes Júnior <junior@Denilsons-Mac.local>
2020-02-16 23:38:21 -0300

    melhorias no código do projeto

--- main.js
+++ main.js
@@ -1,53 +1,38 @@
-let DOG_URL = `https://dog.ceo/api/breeds/image/random`;
-
-const ALL_BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
-
 const dogsImg = document.querySelector(".dogs-img");
-
 const doggos = document.querySelector(".doggos");
-
 const loadingDog = document.querySelector(".dog-load");
-
 const selecionarRacas = document.getElementById("selecionar-raca");
-
-let racas = [];
-let criarTagOption;
 
 loadingDog.style.display = "none";
 
 const addNewDoggo = () => {
     loadingDog.style.display = "block";
-    opcoesRaca = selecionarRacas.options;
-    indexRaca = selecionarRacas.selectedIndex;
-    DOG_URL = `https://dog.ceo/api/breed/${opcoesRaca[indexRaca].text}/images/random`;
-    const promise = fetch(DOG_URL);
-    promise
-    .then((response) => {
-        loadingDog.style.display = "none";
-        const processingPromise = response.json();
-        return processingPromise;
-    })
-    .then((processedResponse) => {
+    DOG_URL = `https://dog.ceo/api/breed/${selecionarRacas.value}/images/random`;
+    fetch(DOG_URL)
+        .then((response) => {
+            loadingDog.style.display = "none";
+            const processingPromise = response.json();
+            return processingPromise;
+        })
+        .then((processedResponse) => {
             dogsImg.src = processedResponse.message;
             dogsImg.alt = "Cute doggo";
         });
 }
 
 const obterRacaCachorros = () => {
-    const promise = fetch(ALL_BREEDS_URL);
-    promise
+    fetch("https://dog.ceo/api/breeds/list/all")
         .then((response) => {
             const processingPromise = response.json();
             return processingPromise;
         })
         .then((processedResponse) => {
-            racas = Object.keys(processedResponse.message);
-            for (let i = 0; i < racas.length; i++) {
-                criarTagOption = document.createElement("option");
-                criarTagOption.textContent = racas[i];
-                criarTagOption.value = racas[i];
+            Object.keys(processedResponse.message).forEach(cao => {
+                const criarTagOption = document.createElement("option");
+                criarTagOption.textContent = cao;
+                criarTagOption.value = cao;
                 selecionarRacas.appendChild(criarTagOption);
-            }
+            }) 
         })
 }
 
